f98fc413dfba625cf1154065514422b3be1e6020
Carlos Vinícius <30941662+carlosviniciusm@users.noreply.github.com>
2022-09-17 23:08:32 -0300

    Update login.php

--- src/view/login/login.php
+++ src/view/login/login.php
@@ -1,11 +1,11 @@
 <div style="text-align: center; margin-top: 25rem">
     <div>
         <label for="login">Login</label>
-        <input type="text" id="login">
+        <input type="text" id="login" />
     </div>
     <div>
         <label for="pass">Senha</label>
-        <input type="text" id="pass">
+        <input type="text" id="pass" />
     </div>
     <div>
         <button>Entrar</button>
