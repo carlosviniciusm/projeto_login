654889a09f80aded907da2cbae70dbdb5f34c0ca
Carlos Vinicius <carlosvinicius@moobitech.com.br>
2022-09-17 23:07:00 -0300

    Altera layout da pagina de login

--- src/view/login/login.php
+++ src/view/login/login.php
@@ -1,5 +1,13 @@
-<label for="login">Login</label>
-<input type="text" id="login">
-<label for="login">Senha</label>
-<input type="text" id="pass">
-<button>Entrar</button>
+<div style="text-align: center; margin-top: 25rem">
+    <div>
+        <label for="login">Login</label>
+        <input type="text" id="login">
+    </div>
+    <div>
+        <label for="pass">Senha</label>
+        <input type="text" id="pass">
+    </div>
+    <div>
+        <button>Entrar</button>
+    </div>
+</div>
